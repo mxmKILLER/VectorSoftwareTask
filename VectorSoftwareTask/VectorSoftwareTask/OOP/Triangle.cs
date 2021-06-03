2ea0528f9de41dcb2f642775b23456e6d52354fc
mxmKILLER <931190@gmail.com>
2021-06-03 14:22:09 +0300

    removed unnecessary usings

--- VectorSoftwareTask/VectorSoftwareTask/OOP/Triangle.cs
+++ VectorSoftwareTask/VectorSoftwareTask/OOP/Triangle.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VectorSoftwareTask.OOP
+﻿namespace VectorSoftwareTask.OOP
 {
     public class Triangle : Shape
     {
